93749977658cf4b49141a589f2c6f167def5545e
jftoh <tohjianfeng@gmail.com>
2024-02-18 23:27:38 +0800

    feat: used speecht5 instead of mms

--- backend/model.py
+++ backend/model.py
@@ -1,14 +1,21 @@
-from transformers import VitsModel, AutoTokenizer, pipeline
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from datasets import load_dataset
 import torch
+import soundfile as sf
 
 class MMSModel:
     def __init__(self):
-        self.model = VitsModel.from_pretrained("facebook/mms-tts-eng")
-        self.tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
-    
-    def convert_to_speech(self, text):
-        inputs = self.tokenizer(text, return_tensors="pt")
+        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
-        with torch.no_grad():
-            output = self.model(**inputs).waveform
-            return output
+    def convert_to_speech(self, text):
+        inputs = self.processor(text=text, return_tensors="pt")
+
+        # load xvector containing speaker's voice characteristics from a dataset
+        embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+        speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+        speech = self.model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=self.vocoder)
+
+        return sf.write("speech.wav", speech.numpy(), samplerate=16000)
